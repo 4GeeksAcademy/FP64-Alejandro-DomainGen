55f08aaa7ddad8002929dec21aab6067ea94e1e7
alejo0022 <156907082+alejo0022@users.noreply.github.com>
2024-04-02 10:56:51 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,6 +5,37 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  func;
+function main() {
+  const pronounAdjList = getPronounAdjList();
+  const pronounAdjNounList = getPronounAdjNounList(pronounAdjList);
+  printPronounAdjNoun(pronounAdjNounList);
+}
+
+const getPronounAdjList = () => {
+  let state = [];
+  const pronounList = ["the", "our"];
+  const AdjList = ["great", "big"];
+  pronounList.forEach((pronoun) => {
+    AdjList.forEach((adj) => {
+      state = [...state, `${pronoun}${adj}`];
+    });
+  });
+  return state;
 };
+
+const getPronounAdjNounList = (pronounAdjList) => {
+  const nounList = ["jogger", "racoon"];
+  return pronounAdjList.reduce((state, pronounAdj) => {
+    nounList.forEach((noun) => {
+      state = [...state, `${pronounAdj}${noun}.com`];
+    });
+    return state;
+  });
+};
+
+const printPronounAdjNoun = (pronounAdjNounList) => {
+  pronounAdjNounList.forEach((pronounAdjNoun) => {
+    console.log(pronounAdjNoun);
+  });
+};
+main();
